592354219254e319f3fa1167ae603e60410207fb
yashvinavadiya <navadiyayashvi72@gmail.com>
2024-12-11 15:39:49 +0530

    css

---
+++ css/spotify login form/index.js
@@ -0,0 +1,4 @@
+let arr = [1, 2, 3, 4, 5];
+let newArr = arr.slice(1, 4); // Extracts elements from index 1 to 3
+console.log(newArr); // [2, 3, 4]
+console.log(arr); // [1, 2, 3, 4, 5] (original array unchanged)
